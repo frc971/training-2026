504fa68af77f212da975e959eda1ba9510dc2fb8
superfalling12 <superfalling12@gmail.com>
2026-06-27 15:44:34 -0700

    finish second lesson task

--- tasks/lesson-02-codebase-sim-task/src/main/java/frc/robot/RobotContainer.java
+++ tasks/lesson-02-codebase-sim-task/src/main/java/frc/robot/RobotContainer.java
@@ -4,7 +4,12 @@
 
 package frc.robot;
 
+import javax.print.attribute.standard.JobHoldUntil;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import frc.robot.subsystems.Pivot;
 
@@ -23,9 +28,17 @@ public class RobotContainer {
   private void configureBindings() {
     // TODO: Bind button 1 to pivot.setTargetAngleDegrees(35.0)
     // TODO: Bind button 2 to pivot.setTargetAngleDegrees(0.0)
+    joystick.button(1).onTrue(Commands.runOnce(()-> pivot.setTargetAngleDegrees(35.0)));
+    joystick.button(2).onTrue(Commands.runOnce(()-> pivot.setTargetAngleDegrees(0.0)));
+
   }
+
+
+
+  
 
   public Command getAutonomousCommand() {
     return null;
   }
+
 }
