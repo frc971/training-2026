39ce236dd5932edcb82a594bcbfbbc8c2a46d8cd
Lightningcode222 <yuvan262626@gmail.com>
2026-06-27 15:49:24 -0700

    Lesson 2

--- tasks/lesson-02-codebase-sim-task/src/main/java/frc/robot/RobotContainer.java
+++ tasks/lesson-02-codebase-sim-task/src/main/java/frc/robot/RobotContainer.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import frc.robot.subsystems.Pivot;
 
@@ -17,12 +18,29 @@ public class RobotContainer {
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+
     configureBindings();
-  }
+    
+
+
+
+
+
+
+
+
+
+    }
+    
 
   private void configureBindings() {
-    // TODO: Bind button 1 to pivot.setTargetAngleDegrees(35.0)
-    // TODO: Bind button 2 to pivot.setTargetAngleDegrees(0.0)
+    
+    
+
+    joystick.button(1).onTrue(Commands.runOnce(() -> pivot.setTargetAngleDegrees(35)));
+    joystick.button(2).onTrue(Commands.runOnce(() -> pivot.setTargetAngleDegrees(0)));
+      
+   
   }
 
   public Command getAutonomousCommand() {
--- tasks/lesson-02-codebase-sim-task/src/main/java/frc/robot/subsystems/Pivot.java
+++ tasks/lesson-02-codebase-sim-task/src/main/java/frc/robot/subsystems/Pivot.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Pivot extends SubsystemBase {
@@ -29,6 +30,9 @@ public class Pivot extends SubsystemBase {
   @Override
   public void periodic() {
     // TODO: Publish current angle, target angle, and atTarget() to SmartDashboard
+    SmartDashboard.putNumber("current angle", getCurrentAngleDegrees());
+    SmartDashboard.putNumber("target angle", getTargetAngleDegrees());
+    SmartDashboard.putBoolean("at target status", atTarget());
   }
 
   @Override
