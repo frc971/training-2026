8d807d3bfab7efbf73f917ade86ecf39eae5e35d
Lightningcode222 <yuvan262626@gmail.com>
2026-07-11 16:02:22 -0700

    lesson3task

--- tasks/lesson-03-superstructure-structure-task/src/main/java/frc/robot/RobotContainer.java
+++ tasks/lesson-03-superstructure-structure-task/src/main/java/frc/robot/RobotContainer.java
@@ -1,20 +1,25 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
+import frc.robot.subsystems.superstructure.Turret;
+import edu.wpi.first.wpilibj2.command.Commands;
+import static edu.wpi.first.units.Units.Degrees;
 
 public class RobotContainer {
   private final CommandJoystick joystick = new CommandJoystick(0);
 
-  // TODO: Add Turret
+  private final Turret turret = new Turret();
 
   public RobotContainer() {
+    Turret turret = new Turret();
     // TODO: Add Turret
 
     configureBindings();
   }
 
   private void configureBindings() {
-    // TODO: When button 1 is pressed turret moves to 45 deg
-    // TODO: When button 2 is pressed turret moves to 200 deg
+    joystick.button(1).onTrue(Commands.runOnce(() -> turret.setPosition(Degrees.of(45))));
+    joystick.button(1).onTrue(Commands.runOnce(() -> turret.setPosition(Degrees.of(200))));
+
   }
 }
