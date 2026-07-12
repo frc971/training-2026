6274ed184f42bd07be9ddefb34d790be76640cc7
Raymond Tian <raymondtian648@gmail.com>
2026-07-11 17:51:06 -0700

    fixed problems mentioned in comments

--- tasks/lesson-03-superstructure-structure-task/src/main/java/frc/robot/RobotContainer.java
+++ tasks/lesson-03-superstructure-structure-task/src/main/java/frc/robot/RobotContainer.java
@@ -1,9 +1,10 @@
 package frc.robot;
 
+import static edu.wpi.first.units.Units.Degrees;
+
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import frc.robot.subsystems.superstructure.Turret;
-import static edu.wpi.first.units.Units.Degrees;
 
 public class RobotContainer {
   private final CommandJoystick joystick = new CommandJoystick(0);
@@ -18,7 +19,7 @@ public class RobotContainer {
 
   private void configureBindings() {
     joystick.button(1).onTrue(Commands.runOnce(() -> turret.setPosition(Degrees.of(45))));
-     joystick.button(2).onTrue(Commands.runOnce(() -> turret.setPosition(Degrees.of(200))));
+    joystick.button(2).onTrue(Commands.runOnce(() -> turret.setPosition(Degrees.of(200))));
     // TODO: When button 1 is pressed turret moves to 45 deg
     // TODO: When button 2 is pressed turret moves to 200 deg
   }
--- tasks/lesson-03-superstructure-structure-task/src/main/java/frc/robot/subsystems/superstructure/Turret.java
+++ tasks/lesson-03-superstructure-structure-task/src/main/java/frc/robot/subsystems/superstructure/Turret.java
@@ -4,15 +4,14 @@ import static edu.wpi.first.units.Units.*;
 
 import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.units.measure.*;
 import frc.robot.lib.superstructure.*;
 
 public class Turret extends AngularSubsystem {
-  public static Angle LOWER_LIMIT = Degrees.of(-180);
-  public static Angle UPPER_LIMIT = Degrees.of(180);
-  
+  public static final Angle LOWER_LIMIT = Degrees.of(-100);
+  public static final Angle UPPER_LIMIT = Degrees.of(100);
+
   public Turret() {
     super(getMotorConfig());
   }
@@ -24,10 +23,10 @@ public class Turret extends AngularSubsystem {
 
     // TODO: For PID, let kP = 100, kI = 0, and kD = 0. Let feedforward
     // gains = 0
-    
+
     tc.Slot0.kP = 100;
     tc.Slot0.kI = 0;
-    tc.Slot0.kD =0;
+    tc.Slot0.kD = 0;
     // TODO: For motion magic let cruise velocity = 10, acceleration = 10
     tc.MotionMagic.MotionMagicCruiseVelocity = 10;
     tc.MotionMagic.MotionMagicAcceleration = 10;
@@ -41,9 +40,6 @@ public class Turret extends AngularSubsystem {
         .TALONFX_CONFIG(tc)
         .FOC(false)
         .build();
-
-
-    
   }
 
   @Override
@@ -51,14 +47,9 @@ public class Turret extends AngularSubsystem {
     // TODO: Normalize the goal position to be within the range of -180 to 180 degrees and clamp
     // the goal position to the physical limits of the turret.
     double targetDegrees = goalPosition.in(Degrees);
-    targetDegrees = MathUtil.inputModulus(goalPosition.in(Degrees),-180,180);
+    targetDegrees = MathUtil.inputModulus(goalPosition.in(Degrees), -95, 95);
     targetDegrees = MathUtil.clamp(targetDegrees, LOWER_LIMIT.in(Degrees), UPPER_LIMIT.in(Degrees));
-    
 
-
-
-
-    
     super.setPositionVoltage(Degrees.of(targetDegrees));
   }
 }
