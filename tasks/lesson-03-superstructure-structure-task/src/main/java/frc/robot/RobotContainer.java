544325bb855789b87c1f94dc28c78da5ee1e9a07
Ming-Yang Jiang <mjiang@Ming-Yangs-MacBook-Pro.local>
2026-07-25 13:25:52 -0700

    lesson 3 task

--- tasks/lesson-03-superstructure-structure-task/src/main/java/frc/robot/RobotContainer.java
+++ tasks/lesson-03-superstructure-structure-task/src/main/java/frc/robot/RobotContainer.java
@@ -1,20 +1,33 @@
 package frc.robot;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import static edu.wpi.first.units.Units.Degrees;
+
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
+import frc.robot.lib.superstructure.MotorConfig;
+import frc.robot.subsystems.superstructure.Turret;
 
 public class RobotContainer {
   private final CommandJoystick joystick = new CommandJoystick(0);
 
   // TODO: Add Turret
-
+  private Turret turret;
   public RobotContainer() {
     // TODO: Add Turret
+    turret = new Turret();
 
     configureBindings();
   }
 
   private void configureBindings() {
     // TODO: When button 1 is pressed turret moves to 45 deg
+    joystick.button(1).onTrue(Commands.runOnce(() -> turret.setPosition(Degrees.of(45))));
     // TODO: When button 2 is pressed turret moves to 200 deg
+    joystick.button(2).onTrue(Commands.runOnce(() -> turret.setPosition(Degrees.of(200))));
+  }
+
+  public Commands getAutonomousCommand() {
+    return null;
   }
 }
--- tasks/lesson-03-superstructure-structure-task/src/main/java/frc/robot/subsystems/superstructure/Turret.java
+++ tasks/lesson-03-superstructure-structure-task/src/main/java/frc/robot/subsystems/superstructure/Turret.java
@@ -2,7 +2,12 @@ package frc.robot.subsystems.superstructure;
 
 import static edu.wpi.first.units.Units.*;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.units.measure.*;
 import frc.robot.lib.superstructure.*;
 
@@ -10,17 +15,39 @@ public class Turret extends AngularSubsystem {
 
   public Turret() {
     // TODO: Fill in constructor
+    super(getMotorConfig());
   }
 
   public static MotorConfig getMotorConfig() {
     TalonFXConfiguration tc = new TalonFXConfiguration();
 
+    
     // TODO: Fill in Motion Magic values, PID and feedforward gains
 
     // TODO: For PID, let kP = 100, kI = 0, and kD = 0. Let feedforward
     // gains = 0
+    tc.Slot0.kS = 0;
+    tc.Slot0.kV = 0;
+    tc.Slot0.kA = 0;
+    tc.Slot0.kG = 0;
+    
+    tc.Slot0.kP = 100;
+    tc.Slot0.kI = 0;
+    tc.Slot0.kD = 0;
 
     // TODO: For motion magic let cruise velocity = 10, acceleration = 10
+    tc.MotionMagic.MotionMagicCruiseVelocity = 10;
+    tc.MotionMagic.MotionMagicAcceleration = 10;
+    tc.MotionMagic.MotionMagicJerk = 0;
+
+    return MotorConfig.builder()
+      .NAME("Turret")
+      .ID(0)
+      .BUS(new CANBus("rio"))
+      .TALONFX_CONFIG(tc)
+      .FOC(false)
+      .build();
+    
 
   }
 
@@ -28,6 +55,15 @@ public class Turret extends AngularSubsystem {
   public void setPosition(Angle goalPosition) {
     // TODO: Normalize the goal position to be within the range of -180 to 180 degrees and clamp
     // the goal position to the physical limits of the turret.
-    super.setPositionVoltage();
+    Angle normalized = Degrees.of(MathUtil.inputModulus(goalPosition.in(Degree), -180, 180));
+    double degnorm = normalized.in(Degrees);
+    Angle clamped = Degrees.of(MathUtil.clamp(degnorm, -95, 95));
+    
+    super.setPositionVoltage(clamped);
+    SmartDashboard.putNumber("Turret Angle", clamped.in(Degrees));
+    SmartDashboard.putNumber("Input Degrees", degnorm);
+    SmartDashboard.putNumber("raw", goalPosition.in(Degrees));
   }
+
+  
 }
